c20682330127c092c1ac788bef490609f3ba6c4a
Carlinnho <carlospure2011@gmail.com>
2026-08-05 15:33:25 -0500

    mejora de fallo de kpis que no liberaba

--- backend-nest/src/users/users.module.ts
+++ backend-nest/src/users/users.module.ts
@@ -4,9 +4,10 @@ import { User } from '../entities/user.entity';
 import { KpiRol } from '../entities/kpi-rol.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { Kpi } from 'src/entities/kpi.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, KpiRol])],
+  imports: [TypeOrmModule.forFeature([User, KpiRol, Kpi])],
   controllers: [UsersController],
   providers: [UsersService],
 })
--- backend-nest/src/users/users.service.ts
+++ backend-nest/src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 import { KpiRol } from '../entities/kpi-rol.entity';
+import { Kpi } from '../entities/kpi.entity';
 import { CacheService } from '../common/cache/cache.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 
@@ -11,6 +12,7 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private usersRepo: Repository<User>,
     @InjectRepository(KpiRol) private rolesRepo: Repository<KpiRol>,
+    @InjectRepository(Kpi) private kpiRepo: Repository<Kpi>,
     private cache: CacheService,
   ) {}
 
@@ -63,12 +65,19 @@ export class UsersService {
     if (dto.kpi_rol_id === null || dto.kpi_rol_id === undefined) {
       user.kpiRolId = null;
       user.kpiAreaId = null;
+
+      await this.kpiRepo.update(
+        { responsableId: userId },
+        { responsableId: null },
+      );
     } else {
       user.kpiRolId = dto.kpi_rol_id;
       user.kpiAreaId = dto.kpi_rol_id === 1 ? null : (dto.kpi_area_id ?? null);
     }
+
     await this.usersRepo.save(user);
     this.cache.invalidatePrefix('users-');
+    this.cache.invalidatePrefix('kpis-');
 
     const reloaded = await this.usersRepo.findOne({
       where: { id: userId },
